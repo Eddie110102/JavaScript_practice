06f988f83bd268c05bf720f77e081f2f1def45db
Eddie110102 <eddiewang@ispan.com.tw>
2025-01-01 20:18:20 +0800

    修改專案結構、README、jQuery寫法

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## [靜態頁面](https://Eddie110102.github.io/JavaScript_practice/01_BMI.html)
 
-## 1. 讓使用者可以輸入自己的體重及身高，根據算出來的 BMI 值顯示下列訊息 [(解答)](./01_BMI.html)
+## 1. 讓使用者可以輸入自己的體重及身高，根據算出來的 BMI 值顯示下列訊息 [(JS 解答)](./01_bmi.html) [(JQ 解答)](./01_bmi-jQuery.html)
 
 > - 公式
 >   - BMI = 體重(公斤)/身高(公尺)的平方
@@ -13,7 +13,7 @@
 >   - 30≦BMI ＜ 35：中度肥胖
 >   - BMI≧35：重度肥胖
 
-## 2. 輸入西元年，判斷是否是閏年 [(解答)](./02_Leap-Year.html)
+## 2. 輸入西元年，判斷是否是閏年 [(JS 解答)](./02_leap-year.html) [(JQ 解答)](./02_leap-year-jQuery.html)
 
 > - 標準
 >   - 西元除以 400 可整除，為閏年
@@ -21,19 +21,21 @@
 >   - 西元除以 4 不可整除，為平年
 >   - 西元除以 100 可整除但除以 400 不可整除，為平年
 
-## 3. 請畫出九九乘法表 [(解答)](./03_Multiplication-Table.html)
+## 3. 請畫出九九乘法表 [(JS 解答)](./03_multiplication-table.html) [(JQ 解答)](./03_multiplication-table-jQuery.html)
 
-![99乘法表](./99table.JPG)
+![99乘法表](./images/99table.JPG)
 
-## 4. 請設計一個 function，傳入攝氏溫度可以轉成華氏溫度，傳入華氏溫度可以轉成攝氏溫度 [(解答)](./04_Temperature-Translate.html)
+## 4. 請設計一個 function，傳入攝氏溫度可以轉成華氏溫度，傳入華氏溫度可以轉成攝氏溫度 [(JS 解答)](./04_temperature-translate.html) [(JQ 解答)](./04_temperature-translate-jQuery.html)
 
 > - 公式
 >   - 華氏要換算成攝氏時，要先減 32 後，再乘以 5/9
 >   - 攝氏要換算成華氏時，要先乘以 9/5，再加上 32。
 
-## 5. 讓使用者輸入生日計算目前幾歲 [(解答)](./05_Birthday.html)
+## 5. 讓使用者輸入生日計算目前幾歲 [(JS 解答)](./05_birthday.html) [(JQ 解答)](./05_birthday-jQuery.html)
 
-## 6. 驗證使用者輸入的密碼，條件如下 [(解答)](./06_Password-regex.html) [(解答\_separate)](./06_Password-regex-separate.html)
+## 6. 驗證使用者輸入的密碼，條件如下 [(JS 解答)](./06_password-regex.html) [(JQ 解答)](./06_password-regex-jQuery.html)
+
+## 6-1. 條件分開顯示 [(JS 解答)](./06_password-regex-separate.html) [(JQ 解答)](./06_password-regex-separate-jQuery.html)
 
 > - 條件
 >   - 8 位數以上
@@ -42,7 +44,7 @@
 >   - 數字
 >   - 符號
 
-## 7. 請使用以下 HTML 結構與預設 CSS 選擇器 ，透過 JavaScript 實作 alert 元件，並且可以點擊「 × 」將其 alert 元件關閉。 [(解答)](./07_alert.html)
+## 7. 請使用以下 HTML 結構與預設 CSS 選擇器 ，透過 JavaScript 實作 alert 元件，並且可以點擊「 × 」將其 alert 元件關閉。 [(JS 解答)](./07_alert.html) [(JQ 解答)](./07_alert-jQuery.html)
 
 > &lt;div class="alert alert-question"><br> > &emsp;&lt;p>確認是否執行動作訊息。&lt;/p><br> > &emsp;&lt;button class="close">×&lt;/button><br> > &lt;/div>
 >
@@ -63,22 +65,25 @@
 > &lt;/div>
 > <hr>
 
-![提示框](./alert.jpg)
+![提示框](./images/alert.jpg)
 
-## 8. Carousel 元件 [(解答)](./08_carousel.html) [(解答_pagination)](./08_carousel-pagination.html)
+## 8. Carousel 元件 [(JS 解答)](./08_carousel.html) [(JQ 解答)](./08_carousel-jQuery.html)
+
+## 8-1. Carousel 元件包含分頁按鈕 [(JS 解答)](./08_carousel-pagination.html) [(JQ 解答)](./08_carousel-pagination-jQuery.html)
 
 > 1. 請自行設計一個寬度 600px、高度 300px 的輪播牆。
 > 2. 請勿使用 Bootstrap 或是其他套件。
 > 3. Carousel 要有點擊切換圖片的功能，並使用下方 data 的圖片呈現在輪播牆中。
 > 4. 輪播牆可增加自動播放圖片功能。
 
-![輪播牆](./carousel.jpg)
+![輪播牆](./images/carousel.jpg)
 
-## 9. 記帳本 [(解答)](./09_account-book.html)
+## 9. 記帳本 [(JS 解答)](./09_account-book.html) [(JQ 解答)](./09_account-book-jQuery.html)
+
 > 1. 請設計一個記帳本。
 > 2. 可以讓使用者透過單行控制項輸入記帳內容。
 > 3. 點擊「 新增 」可以將內容新增到畫面的記帳本區域中。
 > 4. 點擊「 刪除 」可以將單筆資料移除。
 > 5. 備註：至少要有紀錄日期、支出項目與金額。
 
-![記帳本](./accountBook.jpg)
+![記帳本](./images/accountBook.jpg)
--- navbar.js
+++ navbar.js
@@ -1,9 +1,37 @@
-const url = location.href.split('/')
-const nowPage = url[url.length-1]
+const url = location.href.split("/");
+const nowPage = url[url.length - 1];
 
 const nav = document.querySelector("nav");
+// 新增按鈕
+const btnBox = document.createElement("div");
+btnBox.classList.add("btnBox");
+const jsBtn = document.createElement("div");
+jsBtn.textContent = "JS寫法";
+const jqBtn = document.createElement("div");
+jqBtn.textContent = "JQ寫法";
 
-const pages = [
+btnBox.appendChild(jsBtn);
+btnBox.appendChild(jqBtn);
+nav.append(btnBox);
+
+jsBtn.addEventListener("click", function () {
+  this.classList.add("active");
+  jqBtn.classList.remove("active");
+  nav.lastChild.innerHTML = "";
+  changeLink("js");
+});
+
+jqBtn.addEventListener("click", function () {
+  this.classList.add("active");
+  jsBtn.classList.remove("active");
+  nav.lastChild.innerHTML = "";
+  changeLink("jq");
+});
+
+const navBox = document.createElement("div");
+navBox.classList.add("navBox");
+
+const pages_javascript = [
   { link: "01_BMI.html", name: "BMI計算機" },
   { link: "02_Leap-Year.html", name: "閏年計算機" },
   { link: "03_Multiplication-Table.html", name: "九九乘法表" },
@@ -17,22 +45,50 @@ const pages = [
   { link: "09_account-book.html", name: "記帳本" },
 ];
 
-let navbar = "";
-for (let i = 0; i < pages.length; i++) {
-  const result = document.createElement("a");
-  result.textContent = pages[i].name;
-  result.href = pages[i].link;
-  result.alt = pages[i].name;
-  if(pages[i].link == nowPage){
-    result.classList.add('active')
-    const source = document.createElement('a')
-    source.href = `https://github.com/Eddie110102/JavaScript_practice/blob/main/${pages[i].link}`
-    source.textContent = '檢視原始碼'
-    source.alt='檢視原始碼'
-    source.classList.add('source')
-    source.target = '_blank'
-    nav.appendChild(source)
+const pages_jQuery = [
+  { link: "01_BMI-jQuery.html", name: "BMI計算機" },
+  { link: "02_Leap-Year-jQuery.html", name: "閏年計算機" },
+  { link: "03_Multiplication-Table-jQuery.html", name: "九九乘法表" },
+  { link: "04_Temperature-Translate-jQuery.html", name: "溫度轉換器" },
+  { link: "05_Birthday-jQuery.html", name: "年齡計算機" },
+  { link: "06_Password-regex-jQuery.html", name: "密碼驗證" },
+  { link: "06_Password-regex-separate-jQuery.html", name: "密碼驗證分項目" },
+  { link: "07_alert-jQuery.html", name: "彈跳視窗" },
+  { link: "08_carousel-jQuery.html", name: "輪播牆" },
+  { link: "08_carousel-pagination-jQuery.html", name: "輪播牆_pagination" },
+  { link: "09_account-book-jQuery.html", name: "記帳本" },
+];
+
+function changeLink(type) {
+  const data = type == "js" ? pages_javascript : pages_jQuery;
+  for (let i = 0; i < data.length; i++) {
+    const result = document.createElement("a");
+    result.textContent = data[i].name;
+    result.href = data[i].link;
+    result.alt = data[i].name;
+
+    if (data[i].link == nowPage) {
+      console.log("now", nowPage);
+
+      result.classList.add("active");
+      const source = document.createElement("a");
+      source.href = `https://github.com/Eddie110102/JavaScript_practice/blob/main/${data[i].link}`;
+      source.textContent = "檢視原始碼";
+      source.alt = "檢視原始碼";
+      source.classList.add("source");
+      source.target = "_blank";
+      navBox.appendChild(source);
+    }
+    navBox.appendChild(result);
   }
-  nav.appendChild(result);
+  nav.append(navBox);
 }
 
+// 預設js
+if (!nowPage.includes("jQuery")) {
+  jsBtn.classList.add("active");
+  changeLink("js");
+} else {
+  jqBtn.classList.add("active");
+  changeLink("jq");
+}
