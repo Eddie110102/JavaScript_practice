d853e72f1716a3e8f4c1d6fc000ad622aa3ed54e
Eddie110102 <eddiewang@ispan.com.tw>
2025-01-01 17:44:19 +0800

    圖片來源獨立

---
+++ carousel-data.js
@@ -0,0 +1,23 @@
+// 圖片來源
+const data = [
+  {
+    url: "https://www.kpopn.com/upload/fa639a8e1ae64ce8f2c0.jpg",
+    name: "Twice Fancy Cover",
+  },
+  {
+    url: "https://cdn1.wishnote.tw/200/2018/04/24/200_91441_1524554375.jpeg",
+    name: "Twice What is Love MV",
+  },
+  {
+    url: "https://img.ltn.com.tw/Upload/ent/page/800/2021/11/06/phpqJ2lIs.jpg",
+    name: "Twice Formula of Love Cover",
+  },
+  {
+    url: "https://static.popdaily.com.tw/u/202109/4b573e45-9586-41e7-bd5d-c391bbfb2a60.jpg",
+    name: "Twice The Feels Cover",
+  },
+  {
+    url: "https://thebiaslistcom.files.wordpress.com/2024/02/twice-i-got-you.jpeg",
+    name: "Twice I Got You Cover",
+  },
+];
